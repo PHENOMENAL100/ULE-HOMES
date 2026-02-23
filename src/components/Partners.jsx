2cec4be22c89c3d4b4dc7511407047ac4c0ab62e
PHENOMENAL100 <olufemigaius@gmail.com>
2026-02-23 11:20:12 +0000

    Fix production image imports

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,5 +1,12 @@
 import "../App.css";
 
+import techcabal from "../assets/techcabal.png";
+import lsetf from "../assets/lsetf.png";
+import techpoint from "../assets/techpoint.png";
+import antler from "../assets/antler.png";
+import nhm from "../assets/nhm.png";
+import techparley from "../assets/techparley.png";
+
 function Partners() {
   return (
     <section className="partners-section">
@@ -8,12 +15,12 @@ function Partners() {
 
       {/* Logos row */}
       <div className="partners">
-        <img src="/src/assets/techcabal.png" alt="TechCabal" />
-        <img src="/src/assets/lsetf.png" alt="LSETF" />
-        <img src="/src/assets/techpoint.png" alt="TechPoint" />
-        <img src="/src/assets/antler.png" alt="Antler" />
-        <img src="/src/assets/nhm.png" alt="NHM" />
-        <img src="/src/assets/techparley.png" alt="TechParley" />
+        <img src={techcabal} alt="TechCabal" />
+        <img src={lsetf} alt="LSETF" />
+        <img src={techpoint} alt="TechPoint" />
+        <img src={antler} alt="Antler" />
+        <img src={nhm} alt="NHM" />
+        <img src={techparley} alt="TechParley" />
       </div>
     </section>
   );
