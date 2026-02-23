682d4c583a06e503169532cc2418b1959aac0b18
PHENOMENAL100 <olufemigaius@gmail.com>
2026-02-23 11:02:46 +0000

    Fix css import case issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import MainRoutes from "./MainRoutes"; // <-- use the routing file
-import "./Global.css";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
